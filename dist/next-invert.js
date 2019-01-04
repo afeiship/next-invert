255a58d0482d940793f3f440eeb6b8b58e49fde1
afeiship <afeiship@gmail.com>
2019-01-04 11:49:31 +0800

    feat: add testcase use jest

--- dist/next-invert.js
+++ dist/next-invert.js
@@ -1,11 +1,10 @@
-(function () {
-
+(function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
 
-  nx.invert = function (inObject) {
+  nx.invert = function(inObject) {
     var map = {};
-    nx.each(inObject, function (key, value) {
+    nx.each(inObject, function(key, value) {
       if (map[value] === undefined) {
         map[value] = [key];
       } else {
@@ -18,5 +17,4 @@
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = nx.invert;
   }
-
-}());
+})();
--- test/test.js
+++ test/test.js
@@ -1,18 +1,25 @@
-var assert = require('assert');
 var nx = require('next-js-core2');
 require('../src/next-invert');
 
-describe('next/invert', function () {
+test('next/invert', function() {
+  var object = { a: 1, b: 2, c: 1 };
+  var rs1 = nx.invert(object);
 
-  it('nx.invert with multiple', function () {
-    var object = { 'a': 1, 'b': 2, 'c': 1 };
-    var rs1 = nx.invert( object );
+  // console.log(rs1);
 
-    console.log(
-      rs1
-    );
+  expect(rs1).toEqual({ '1': ['a', 'c'], '2': ['b'] });
+});
 
-    assert.deepEqual(rs1, { '1': ['a', 'c'], '2': ['b'] });
+test('next/invert', function() {
+  var object = { a: 1, b: 2, c: 1, items: [], items1: [], items2: [{ k1: 'v1' }] };
+  var rs1 = nx.invert(object);
+
+  // console.log(rs1);
+
+  expect(rs1).toEqual({
+    '1': ['a', 'c'],
+    '2': ['b'],
+    '': ['items', 'items1'],
+    '[object Object]': ['items2']
   });
-
 });
